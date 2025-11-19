3289ce878150dd0c1b81d15b5a723c517e74dcf8
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-20 00:04:48 +0530

    Update variables.tf

--- terraform/modules/bastion/variables.tf
+++ terraform/modules/bastion/variables.tf
@@ -1,5 +1,24 @@
-variable "subnet_id" {}
-variable "ami_id" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "bastion_sg_id" {}
+variable "subnet_id" {
+  description = "The subnet ID where the bastion host will be launched"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the bastion host"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for the bastion host"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key name for the bastion host"
+  type        = string
+}
+
+variable "bastion_sg_id" {
+  description = "Security Group ID for the bastion host"
+  type        = string
+}
