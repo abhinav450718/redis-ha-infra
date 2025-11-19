18aeb654c23f1353e1c6108522219cc9009f87f8
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-20 00:05:11 +0530

    Clean up bastion module by removing unused variables
    Removed unnecessary variables and outputs from bastion module.

--- terraform/modules/bastion/main.tf
+++ terraform/modules/bastion/main.tf
@@ -1,9 +1,3 @@
-variable "subnet_id" {}
-variable "ami_id" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "bastion_sg_id" {}
-
 resource "aws_instance" "bastion" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
@@ -16,11 +10,4 @@ resource "aws_instance" "bastion" {
     Name = "bastion-host"
     Role = "bastion"
   }
-}
-
-output "bastion_id" {
-  value = aws_instance.bastion.id
-}
-output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
 }
