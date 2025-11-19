170e7ac9b237e6e94f4b26b06cf7d7262042af74
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-20 01:58:55 +0530

    Update variables.tf

--- terraform/modules/redis/variables.tf
+++ terraform/modules/redis/variables.tf
@@ -4,5 +4,4 @@ variable "instance_type" {}
 variable "key_name" {}
 variable "db_sg_id" {}
 variable "name" {}        # e.g. "redis-master" or "redis-replica"
-variable "role_type" {}   # e.g. "master" / "replica"
-
+variable "role_type" {}   # e.g. "master" or "replica"
