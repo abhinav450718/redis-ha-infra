45c9505c090f102542dfcfb771285621e87e32c4
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-19 23:54:40 +0530

    Add Terraform variables for region, VPC, and instance

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,10 @@
+variable "region" { default = "sa-east-1" }
+variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "instance_type" { default = "t3.micro" }
+variable "ssh_key_name" { default = "redis-key" }
+variable "ami_owner" { default = "099720109477" } # canonical ubuntu
+
+
+
+
 
