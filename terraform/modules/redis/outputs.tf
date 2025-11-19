83364a3a0460726d25e2e2de4a9f58a3c05df30b
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-20 01:00:03 +0530

    Update outputs.tf

--- terraform/modules/redis/outputs.tf
+++ terraform/modules/redis/outputs.tf
@@ -1,1 +1,16 @@
+output "id" {
+  value = aws_instance.redis.id
+}
+
+output "private_ip" {
+  value = aws_instance.redis.private_ip
+}
+
+output "name" {
+  value = var.name
+}
+
+output "role_type" {
+  value = var.role_type
+}
 
