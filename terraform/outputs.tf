77565e6540a8cc9cb2552c0f91eda9540bc7ad96
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-19 10:33:59 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,34 @@
+output "bastion_public_ip" {
+  value = module.bastion.bastion_public_ip
+}
+
+output "redis_master_private_ip" {
+  value = module.redis_master.private_ip
+}
+
+output "redis_replica_private_ip" {
+  value = module.redis_replica.private_ip
+}
+
+output "private_key_path" {
+  value = "${path.module}/redis_key.pem"
+}
+
+output "ssh_to_bastion" {
+  value = "ssh -i ${path.module}/redis_key.pem ubuntu@${module.bastion.bastion_public_ip}"
+}
+
+output "ssh_to_redis_master_via_bastion" {
+  value = "ssh -i ${path.module}/redis_key.pem -J ubuntu@${module.bastion.bastion_public_ip} ubuntu@${module.redis_master.private_ip}"
+}
+
+output "ssh_to_redis_replica_via_bastion" {
+  value = "ssh -i ${path.module}/redis_key.pem -J ubuntu@${module.bastion.bastion_public_ip} ubuntu@${module.redis_replica.private_ip}"
+}
+
+output "redis_cli_master" {
+  value = "redis-cli -h ${module.redis_master.private_ip} -p 6379"
+}
+output "redis_cli_replica" {
+  value = "redis-cli -h ${module.redis_replica.private_ip} -p 6379"
+}
